8b779402c3f8b6de48ddd9ec963dffe88b118690
Henrique Rauen <henrique_rauen@yahoo.com.br>
2023-06-14 10:47:40 +0200

    Adding main.py and minor corrections to the game logic

---
+++ main.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+
+#Created by Henrique Rauen (rickgithub@hsj.email)
+#Last Modified: Wed Jun 14 10:32:42 2023
+import utils.game as game
+hang = game.Hangman()
+hang.start_game()
--- utils/game.py
+++ utils/game.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python
 
 #Created by Henrique Rauen (rickgithub@hsj.email)
-#Last Modified: Wed Jun 14 10:18:16 2023
+#Last Modified: Wed Jun 14 10:45:26 2023
 class Hangman:
-    available_letters = ["abcdefghijklmnopqrstuvwxyz"]
+    available_letters = "abcdefghijklmnopqrstuvwxyz"
     def __init__(self):
         self._possible_words = ["crazyness"]
-        *self._word_to_find = self._possible_words
+        *self._word_to_find, = self._possible_words[0]
         self._lives = 5
         self._correctly_guessed_letters = ["_"] * len(self._word_to_find)
         self._wrongly_guessed_letters = []
@@ -14,41 +14,42 @@ class Hangman:
         self._error_count = 0
 
     def start_game(self):
+        print(self._word_to_find)
+        print(self._correctly_guessed_letters)
         while self._lives > 0:
             if "_" in self._correctly_guessed_letters:
-                play()
+                self._play()
             else:
-                _well_played()
+                self._well_played()
                 return None
-        _game_over()
+        self._game_over()
 
     def _well_played(self):
-        print (f"You found the word {self._word_to_find} in {self._turn_count}
-               turns with {self._error_count}")
+        print (f"You found the word {self._word_to_find} in {self._turn_count} turns with {self._error_count}")
 
     def _game_over(self):
         print("game over...")
 
     def _play(self):
         guessed_letter = input("Please guess a letter: ")
-        if _guess_validity(guessed_letter):
+        if self._guess_validity(guessed_letter):
             if guessed_letter in self._word_to_find:
-               _update_hangman(guessed_letter)
+               self._update_hangman(guessed_letter)
             else:
                 self._wrongly_guessed_letters.append(guessed_letter)
                 self._error_count += 1
                 self._lives += -1
         else:
-            _play()
+            self._play()
 
     def _update_hangman(self,letter):
         for index, l in enumerate(self._word_to_find):
             if l == letter:
                 self._correctly_guessed_letters[index] = letter
 
-    def _guess_validity(self,x)
+    def _guess_validity(self,x):
         x.lower()
-        if x.len() == 1 and x in available_letters:
+        if len(x) == 1 and x in self.available_letters:
             if (x not in self._wrongly_guessed_letters and
                 x not in self._correctly_guessed_letters):
                 validity = True
